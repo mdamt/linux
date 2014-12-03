c91d34b9840de7f45834b090ef51e2eea9894a74
Mohammad Anwari <mdamt@mdamt.net>
2014-12-03 19:59:23 +0700

    Use defconfig to build

--- blankon/envsetup.sh
+++ blankon/envsetup.sh
@@ -3,3 +3,6 @@ export PATH=$PATH:`pwd`/bin
 popd
 export ARCH=arm
 export CROSS_COMPILE=arm-eabi-
+cp blankon/config arch/arm/configs/blankon-rk3188_defconfig
+echo "make blankon-rk3188_defconfig"
+echo "make -j9 zImage dtbs"
