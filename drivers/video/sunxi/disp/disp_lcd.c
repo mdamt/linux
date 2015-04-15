34c4e03e38c95fe67344e12b0b55ab45f6bcfc36
Mohammad Anwari <mdamt@mdamt.net>
2015-04-15 23:05:33 +0700

    Cubiescreen driver

--- drivers/video/sunxi/disp/disp_lcd.c
+++ drivers/video/sunxi/disp/disp_lcd.c
@@ -648,8 +648,32 @@ LCD_get_sys_config(__u32 sel, __disp_lcd_cfg_t *lcd_cfg)
 }
 
 
-void
-LCD_OPEN_FUNC(__u32 sel, LCD_FUNC func, __u32 delay)
+
+void LCD_delay_us(__u32 us) 
+{
+volatile __u32 time;
+//#ifdef __LINUX_OSAL__
+        udelay(us);
+//#endif
+//#ifdef __BOOT_OSAL__
+        
+        for(time = 0; time < (us*700/10);time++);//assume cpu runs at 700Mhz,10 clock one cycle
+//#endif
+}
+
+void LCD_delay_ms(__u32 ms) 
+{
+//#ifdef __LINUX_OSAL__
+        __u32 timeout = ms*HZ/1000;
+        set_current_state(TASK_INTERRUPTIBLE);
+        schedule_timeout(timeout);
+//#endif
+#ifdef __BOOT_OSAL__
+        wBoot_timer_delay(ms);//assume cpu runs at 1000Mhz,10 clock one cycle
+#endif
+}
+
+void LCD_OPEN_FUNC(__u32 sel, LCD_FUNC func, __u32 delay)
 {
 	open_flow[sel].func[open_flow[sel].func_num].func = func;
 	open_flow[sel].func[open_flow[sel].func_num].delay = delay;
